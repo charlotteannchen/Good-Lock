6a12d731bf87dd3f1a49597350b7d4e1a2826042
charlotteannchen <r12922029@ntu.edu.tw>
2024-09-11 19:46:31 +0800

    Add Searching mode

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'http://172.20.10.14/receive_device_id'
-data = {'id': 'goodlock2u'}  # Replace 'your_device_id_here' with a test device ID
-
-response = requests.post(url, data=data)
-
-print(response.status_code)
-print(response.text)
